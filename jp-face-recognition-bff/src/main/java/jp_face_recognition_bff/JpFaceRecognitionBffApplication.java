3dca8edde643cb7d13ae8ba7f4de05fd463ac90c
joaopedromendesjava <joaopedromendessilva@gmail.com>
2026-03-14 18:13:23 -0300

    feat: create project bff first commit

---
+++ jp-face-recognition-bff/src/main/java/jp_face_recognition_bff/JpFaceRecognitionBffApplication.java
@@ -0,0 +1,13 @@
+package jp_face_recognition_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpFaceRecognitionBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpFaceRecognitionBffApplication.class, args);
+	}
+
+}
---
+++ jp-face-recognition-bff/src/test/java/jp_face_recognition_bff/JpFaceRecognitionBffApplicationTests.java
@@ -0,0 +1,13 @@
+package jp_face_recognition_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpFaceRecognitionBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
